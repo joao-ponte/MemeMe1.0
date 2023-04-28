ea9c9669145c857a518b0bacb316b5a88f45ba35
Joao Ponte <joao_ponte@msn.com>
2023-04-28 11:37:06 +0100

    Refactor code to create new classes: "MemedTableView Cell" and "MemedCollectionViewCell", and assign identification to the cells. Additionally, add an outlet for the image on collections.

---
+++ MemeMe1/MemedCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemedCollectionViewCell.swift
+//  MemeMe1
+//
+//  Created by João Ponte on 28/04/2023.
+//
+
+import UIKit
+
+class MemedCollectionViewCell: UICollectionView {
+
+    @IBOutlet weak var memedImageView: UIImageView!
+    
+    
+    
+}
---
+++ MemeMe1/MemedTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MemedTableViewCell.swift
+//  MemeMe1
+//
+//  Created by João Ponte on 28/04/2023.
+//
+
+import UIKit
+
+class MemedTableViewCell: UITableViewDataSource {
+    
+}
